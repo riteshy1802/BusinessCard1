5d631c8e590f19568959d3fb92d18a175a220ee6
riteshy1802 <143412534+riteshy1802@users.noreply.github.com>
2023-11-02 16:58:08 +0530

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,11 +3,11 @@ import React from "react"
 export default function Footer(){
     return(
         <div className="links">
-            <a href="https://twitter.com/Ritesh182006"><i class='bx bxl-twitter'></i></a>
-            <a href="#"><i class='bx bxl-meta' ></i></a>
-            <a href="#"><i class='bx bxl-instagram-alt' ></i></a>
-            <a href="https://www.linkedin.com/in/ritesh-yadav-2a5a03289/"><i class='bx bxl-linkedin-square' ></i></a>
-            <a href="https://github.com/riteshy1802"><i class='bx bxl-github'></i></a>
+            <a href="https://twitter.com/Ritesh182006" target="blank"><i class='bx bxl-twitter'></i></a>
+            <a href="https://www.facebook.com/profile.php?id=61553014022540" target="blank"><i class='bx bxl-meta' ></i></a>
+            <a href="https://www.instagram.com/ritesh7826yadav/" target="blank"><i class='bx bxl-instagram-alt' ></i></a>
+            <a href="https://www.linkedin.com/in/ritesh-yadav-2a5a03289/" target="blank"><i class='bx bxl-linkedin-square' ></i></a>
+            <a href="https://github.com/riteshy1802" target="blank"><i class='bx bxl-github'></i></a>
         </div>
     )
 }
